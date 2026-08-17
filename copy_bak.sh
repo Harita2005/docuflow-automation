95b791173cda9fd866f95809beb37f3bbdade9b0
Harita2005 <harita010905@gmail.com>
2026-08-17 14:41:11 +0530

    chore: update copy_bak.sh to copy directly to container data folder

--- copy_bak.sh
+++ copy_bak.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "Creating backup folder inside docuflow-db container..."
-docker exec -u 0 docuflow-db mkdir -p /var/opt/mssql/backup
-echo "Copying DocuFlowDB.bak into container..."
-docker cp /opt/docflow/docuflow-automation/DocuFlowDB.bak docuflow-db:/var/opt/mssql/backup/
+echo "Copying DocuFlowDB.bak directly into the mssql/data folder..."
+docker cp /opt/docflow/docuflow-automation/DocuFlowDB.bak docuflow-db:/var/opt/mssql/data/
 echo "Setting correct file permissions..."
-docker exec -u 0 docuflow-db chown -R mssql:mssql /var/opt/mssql/backup
-echo "Done! The file is now copied to /var/opt/mssql/backup/DocuFlowDB.bak inside the database container."
+docker exec -u 0 docuflow-db chown mssql:mssql /var/opt/mssql/data/DocuFlowDB.bak
+echo "Done! The file is now copied to /var/opt/mssql/data/DocuFlowDB.bak inside the database container."
